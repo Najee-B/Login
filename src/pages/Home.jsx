f6eadb85502093a7ef502b231231b2617094be79
Najee-B <muhammednajeeb632@gmail.com>
2023-06-25 15:40:40 +0530

    second commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,8 +4,8 @@ import spot from '../assets/spot.png'
 
 function Home() {
   return (
-    <div class="container">
-    <nav class="navi"><div class="imgd"><img src={spot} alt=""/></div>
+    <div className="container">
+    <nav className="navi"><div class="imgd"><img src={spot} alt=""/></div>
         
         <ul>
         <li>Premium</li>
@@ -15,14 +15,15 @@ function Home() {
         <li>Sign up</li>
         <li>Log in</li>
     </ul></nav>
-    <div class="top_circle">
+    <div className="top_circle">
+      <div className="inner">
         <h2>Music for everyone.</h2>
         <p className='p'>Millions of songs. No credit card needed.</p>
-        <button class="spotify">GET SPOTIFY FREE</button>
-    </div>
-        <div class="left_circle"></div>
-        <div class="middle_circle"></div>
-        <div class="right_circle"></div>
+        <button className="spotify">GET SPOTIFY FREE</button>
+    </div></div>
+        <div className="left_circle"></div>
+        <div className="middle_circle"></div>
+        <div className="right_circle"></div>
         
     </div> 
   )
